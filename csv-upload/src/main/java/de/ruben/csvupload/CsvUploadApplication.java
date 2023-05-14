4b2c9a0d31a2f39d1cab9cf483a8c37ec1aeaba9
Ruben Kraus <s80350@htw-dresden.de>
2023-05-14 12:24:01 +0200

    deleted shit

--- csv-upload/src/main/java/de/ruben/csvupload/CsvUploadApplication.java
+++ csv-upload/src/main/java/de/ruben/csvupload/CsvUploadApplication.java
@@ -2,8 +2,10 @@ package de.ruben.csvupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
+@ComponentScan({"de.ruben.csvupload.repository"})
 public class CsvUploadApplication {
 
 	public static void main(String[] args) {
--- csv-upload/src/main/java/de/ruben/csvupload/controller/CSVUploadController.java
+++ csv-upload/src/main/java/de/ruben/csvupload/controller/CSVUploadController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import de.ruben.csvupload.service.CSVService;
 import de.ruben.csvupload.model.Student;
 
 @Controller
+@CrossOrigin("localhost")
 public class CSVUploadController {
 
     @Autowired
